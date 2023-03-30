e388bf20f3a12def081690e346e30ffda8948930
Nsamba Isaac <nsambaisaac@Nsambas-MacBook-Pro.local>
2023-03-30 09:20:07 +0300

    setting up static files

--- zend_blog/settings.py
+++ zend_blog/settings.py
@@ -31,7 +31,6 @@ environ.Env.read_env()
 
 SECRET_KEY = env('SECRET_KEY')
 
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -138,6 +137,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
