4b94d1e77719e7b77c4611d05f3395129b03f338
Nsamba Isaac <nsambaisaac@Nsambas-MacBook-Pro.local>
2023-03-30 12:17:15 +0300

    adding allowed hosts

--- zend_blog/settings.py
+++ zend_blog/settings.py
@@ -95,8 +95,8 @@ DATABASES = {
         'NAME': env('DATABASE_NAME'),
         'USER': env('DATABASE_USER'),
         'PASSWORD': env('DATABASE_PASS'),
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'HOST': env('DATABASE_HOST'),
+        'PORT': env('DATABASE_PORT'), 
     }
 }
 
