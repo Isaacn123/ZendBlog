854b491c3fde17bbf09b1cdfc4cebb980d020d7a
Nsamba Isaac <nsambaisaac@Nsambas-MacBook-Pro.local>
2023-04-05 16:51:18 +0300

    adding static

--- zend_blog/settings.py
+++ zend_blog/settings.py
@@ -158,7 +158,7 @@ USE_TZ = True
 # STATIC_ROOT = os.path.join(BASE_DIR, "assets")
 # if not DEBUG:
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, "assets")
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
     
     # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
